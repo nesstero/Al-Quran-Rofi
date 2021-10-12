639e08bcf0052da4ff70ab63e9a50936616c7124
Nestero <nestero@tuta.io>
2021-10-12 08:59:39 +0700

    add option terjemahan / tafsir

--- Quran.py
+++ Quran.py
@@ -1,26 +1,9 @@
 #! /usr/bin/env python
 
-from os import environ as env
 from dynmen.rofi import Rofi
 from bs4 import BeautifulSoup
 from assets import *
-from configparser import ConfigParser as conf
 import pyperclip as c
-
-user = env["USER"]
-cfg = conf()
-config_file = f"/home/{user}/.config/quran-rofi.ini"
-
-if not p.isfile(config_file):
-    cfg.add_section("Font")
-    cfg.set("Font", "font", "Al Qalam Quran Majeed 20")
-    
-    with open(config_file, "w") as cfg_ini:
-        cfg.write(cfg_ini)
-else:
-    cfg.read(config_file)
-
-font_quran = cfg["Font"]["font"]
 
 al_quran = BeautifulSoup(quran, "xml")
 terjemahan_id = BeautifulSoup(terjemahan, "xml")
@@ -83,10 +66,13 @@ def Menu():
         AyatdanTerjemahan()
         
         def CopyAyat():
-            ayat_dan_terjemahan = f"Ayat {index_ayat} Surat {nama_surat}: \n {ayat} \n Tafsir Jalalayn: \n {terjemahan_ayat}"
+            if opt_tafsir == "true":
+                ayat_dan_terjemahan = f"Ayat {index_ayat} Surat {nama_surat}: \n {ayat} \n Tafsir Jalalayn: \n {terjemahan_ayat}"
+            else:    
+                ayat_dan_terjemahan = f"Ayat {index_ayat} Surat {nama_surat}: \n {ayat} \n Terjemahan: \n {terjemahan_ayat}"
             c.copy(ayat_dan_terjemahan)
 
-        l_menu_tampil_ayat = {"Tafsir Jalalayn", "Ayat"}
+        l_menu_tampil_ayat = {"Terjemahan/Tafsir", "Ayat"}
         l_menu_tampil_ayat = sorted(l_menu_tampil_ayat)
         def TampilAyat():
             global p_tampil_ayat
@@ -102,14 +88,17 @@ def Menu():
         TampilAyat()
 
         def TampilTerjemahan():
-            if p_tampil_ayat == "Tafsir Jalalayn":
+            if p_tampil_ayat == "Terjemahan/Tafsir":
                 l_menu_tampil_terjemahan = {"Ayat", "Surat", "Copy"}
                 l_menu_tampil_terjemahan = sorted(l_menu_tampil_terjemahan)
                 if panjang_terjemahan >= 1000:
                     menu_tampil_terjemahan = Rofi(lines=1, columns=3, width=1270, hide_scrollbar=True)
                 else:
                     menu_tampil_terjemahan = Rofi(lines=1, columns=3, width=700, hide_scrollbar=True)
-                menu_tampil_terjemahan.prompt = f"Tafsir Jalalayn Surat {nama_surat} Ayat {index_ayat}"
+                if opt_tafsir == "true": 
+                    menu_tampil_terjemahan.prompt = f"Tafsir Jalalayn Surat {nama_surat} Ayat {index_ayat}"
+                else:
+                    menu_tampil_terjemahan.prompt = f"Terjemahan Surat {nama_surat} Ayat {index_ayat}"
                 menu_tampil_terjemahan.mesg = terjemahan_ayat
                 menu_tampil_terjemahan = menu_tampil_terjemahan(l_menu_tampil_terjemahan)
                 p_tampil_terjemahan = menu_tampil_terjemahan.selected
