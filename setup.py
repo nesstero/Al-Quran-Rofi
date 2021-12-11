0b5b840564cea1d264991fcccdb9081e7a8cca10
Nestero <nestero@mail.com>
2021-12-12 02:06:57 +0700

    add desktop file

--- setup.py
+++ setup.py
@@ -4,13 +4,18 @@ from setuptools import find_packages, setup
 with open("README.md", "r") as des:
     l_desc = des.read()
 
+data_files = [('share/applications', ['quran_rofi/assets/quran.desktop']),
+              ('share/icons', ['quran_rofi/assets/quran.png']),
+            ]
+
 setup(
     name='quran_rofi',
     packages=find_packages(),
-    version='0.1.5',
+    version='0.1.6',
     entry_points={'console_scripts': ['quran-rofi = quran_rofi.quran:Menu']},
     include_package_data=True,
     package_data={'': ['source/*.xml']},
+    data_files=data_files,
     description='Al-Quran dengan Terjemahan / Tafsir Jalalayn',
     url='https://gitlab.com/nesstero/Al-Quran-Rofi',
     license='MIT',
